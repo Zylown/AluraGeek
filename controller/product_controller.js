6f6c1bcf4b22f2885614a485d44aa5a73dc74e08
Sevastian Caballero <sevastiancaballero321@gmail.com>
2023-08-25 14:27:59 -0500

    crud finish

---
+++ controller/product_controller.js
@@ -0,0 +1,83 @@
+import { productService } from "../service/product-service.js";
+/*
+//MOSTRAR PRODUCTOS
+const crearNuevaLinea = (imagen, producto, precio) => {
+  const linea = document.createElement("li");
+  const contenido = `
+      <a href="#">
+        <img
+          src="${imagen}"
+          alt="Producto"
+          class="image--producto"
+        />
+      </a>
+      <h3>${producto}</h3>
+      <h4>$ ${precio}</h4>
+      <a href="#">Ver producto</a>
+    `;
+  linea.innerHTML = contenido;
+  return linea;
+};
+
+const ul = document.querySelector("[data-ul]");
+
+productService
+  .listaProductos()
+  .then((data) => {
+    data.forEach((elemento) => {
+      const nuevaLinea = crearNuevaLinea(
+        elemento.url,
+        elemento.nombre,
+        elemento.precio
+      ); //trae los datos del db.json
+      ul.appendChild(nuevaLinea);
+    });
+    //console.log(data);
+  })
+  .catch((e) => {
+    console.log("Error: " + e);
+  });*/
+
+/*FILTRAR NOMRBE DE PRODUCTO*/
+
+const searchInput = document.querySelector(".input__search");
+const productList = document.getElementById("product__list");
+
+const filtrarProductos = (term) => {
+  productService.listaProductos().then((productos) => {
+    const productosFiltrados = productos.filter((producto) =>
+      producto.nombre.toLowerCase().includes(term.toLowerCase())
+    );
+    mostrarProductos(productosFiltrados);
+  });
+};
+
+const mostrarProductos = (productos) => {
+  productList.innerHTML = "";
+
+  productos.forEach((producto) => {
+    const productoElement = document.createElement("li");
+    productoElement.innerHTML = `
+    <a href="#">
+      <img
+        src="${producto.url}"
+        alt="Producto"
+        class="image--producto"
+      />
+    </a>
+    <h3>${producto.nombre}</h3>
+    <h4>$ ${producto.precio}</h4>
+    <a href="#">Ver producto</a>
+  `;
+    productList.appendChild(productoElement);
+  });
+};
+
+searchInput.addEventListener("input", (event) => {
+  const searchTerm = event.target.value;
+  filtrarProductos(searchTerm);
+});
+
+productService.listaProductos().then((productos) => {
+  mostrarProductos(productos);
+});
--- service/product-service.js
+++ service/product-service.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const crearNuevaLinea = (imagen, producto, precio) => {
   const linea = document.createElement("li");
   const contenido = `
@@ -51,3 +52,55 @@ listaProductos()
   .catch((e) => {
     console.log("Error: " + e);
   });
+=======
+const listaProductos = () =>
+  //a mi respuesta le da un formato de json
+  //https://alurageek-api-jade.vercel.app/producto
+  fetch("http://localhost:3000/producto").then((respuesta) => respuesta.json());
+
+//http://localhost:3000/producto
+const crearCliente = (url, nombre, precio, descripcion) => {
+  //https://alurageek-api-jade.vercel.app/producto
+  return fetch("http://localhost:3000/producto", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ url, nombre, precio, descripcion, id: uuid.v4() }),
+  });
+};
+
+const eliminarProducto = (id) => {
+  //https://alurageek-api-jade.vercel.app/producto
+  return fetch(`http://localhost:3000/producto/${id}`, {
+    method: "DELETE",
+  });
+};
+
+const detalleProducto = (id) => {
+  //https://alurageek-api-jade.vercel.app/producto
+  return fetch(`http://localhost:3000/producto/${id}`)
+    .then((respuesta) => respuesta.json())
+    .catch((err) => console.log("Error : " + err));
+};
+
+const actualizarProducto = (url, nombre, precio, descripcion, id) => {
+  //http://localhost:3000/producto
+  //https://alurageek-api-jade.vercel.app/producto
+  return fetch(`http://localhost:3000/producto/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ url, nombre, precio, descripcion }),
+  })
+    .then((respuesta) => respuesta)
+    .catch((err) => console.log(err));
+};
+
+export const productService = {
+  listaProductos,
+  crearCliente,
+  eliminarProducto,
+  detalleProducto,
+  actualizarProducto,
+};
+>>>>>>> Stashed changes
